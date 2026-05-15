3f09793c69abf6408ea98aa8a2c95d97b7043cd5
marcobarbagli-coder <marco.barbagli@studenti.itisarezzo.it>
2026-05-15 17:15:51 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,5 @@
-
+public class MioProgramma {                // Classe
+    public static void main(String[] args) { // Metodo main
+        System.out.println("Ciao, mondo!");  // Istruzione
+    }
+}
