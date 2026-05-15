e093e73b54a68204d6f31513ed69b72136426b0b
gog-code <aksjackbuxai@gmail.com>
2026-05-15 18:09:42 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {                // Classe
     public static void main(String[] args) { // Metodo main
         new GUI();// Istruzione
         //sono gay
+
+        //sei 
     }
 }
