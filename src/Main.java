10f105d760e374e40ae6d9eb2c83b1ed716c8e30
marcobarbagli-coder <marco.barbagli@studenti.itisarezzo.it>
2026-05-19 10:13:06 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {                // Classe
     public static void main(String[] args) { // Metodo main
         new GUI();// Istruzione
-        //sono gay
+        System.out.println("Hello World!"); // Istruzione
     }
 }
